f18a7e4381d6d0f7d88970d5be90be2d1f20d024
Rasaq Ogunsakin <39163087+femilv@users.noreply.github.com>
2022-02-26 12:33:53 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "myJune2021Key" 
+    default     =  "MyEC2key" 
 }
 
 variable "instance_type" { 
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "jenkins-sgroup-dec-2021" 
+    default     = "jenkins-sgroup-jan-2022" 
 }
 
 variable "tag_name" { 
@@ -24,28 +24,11 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-020db2c14939a8efb" 
+    default     = "ami-03a0c45ebc70f98ea" 
 }
 variable "versioning" {
     type        = bool
     description = "(Optional) A state of versioning."
     default     = true
 }
-variable "acl" {
-    type        = string
-    description = " Defaults to private "
-    default     = "private"
-}
-variable "bucket_prefix" {
-    type        = string
-    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "my-s3bucket-"
-}
-variable "tags" {
-    type        = map
-    description = "(Optional) A mapping of tags to assign to the bucket."
-    default     = {
-        environment = "DEV"
-        terraform   = "true"
-    }
-}
+
