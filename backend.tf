93ad1511ac489c9d56dc07abc5bbffddbb820b8d
Rasaq Ogunsakin <39163087+femilv@users.noreply.github.com>
2022-02-26 13:19:18 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket = "my-jan-s3-bucket"
-    key = "main"
-    region = "us-east-2"
-    dynamodb_table = "my-jan-dynamodb-table"
+  backend "local" {
+    path = "relative/path/to/terraform.tfstate"
   }
 }
